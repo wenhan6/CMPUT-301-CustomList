ad4b84de60b034ddc1bd5f02a655d0c6e7acb734
wenhan6 <wenhan6@ualberta.ca>
2022-03-09 20:37:33 -0700

    fixed deleteCityTest

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -62,6 +62,12 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void deleteCity(City city) {
+        if (cities.contains(city)) {
+            cities.remove(city);
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
         return;
     }
 }
